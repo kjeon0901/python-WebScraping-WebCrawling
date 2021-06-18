a977d31541fda933741541b08c12128e8dcc36b0
kjeon0901 <kjeon0901@naver.com>
2021-06-18 09:49:37 +0900

    add - rq.get(url), response.status_code add - server, page

--- all_codes_per_chapters/ch6/6.4.py
+++ all_codes_per_chapters/ch6/6.4.py
@@ -4,3 +4,18 @@ url = "https://pjt3591oo.github.io"
 
 res = rq.get(url)
 print(res)
+print(res.status_code)
+'''
+<Response [200]>
+200
+
+
+내가 어떤 요청을 하고 그게 수행되면 연산 결과 or 상태메시지에 대한 '응답 코드'가 온다. 
+<Response [200]>    - 정상적으로 작동했다. 
+                    - ex_ 로그인할 때 id, password를 입력하고 로그인 버튼 누르면, id, password를 담은 메시지가 request로 가고, 회원정보에 해당되면 200 반환
+<Response [201]>    - 정상적으로 저장되었다. 
+<Response [401]>    - 권한이 없다. 
+... 수많은 응답 코드가 있으므로 외우지 말고 그때그때 찾아보자!
+
+=> 웹크롤링 하는 도중 문제 해결이 필요하다면 웹에 대한 이런 기본 지식이 필요하다. 
+'''
--- all_codes_per_chapters/ch6/6.6.py
+++ all_codes_per_chapters/ch6/6.6.py
@@ -1,8 +1,25 @@
 import requests as rq
 
-url = "https://pjt3591oo.github.io/a"
+url1 = "https://aldkfja.com/kjeon0901/a" # https://aldkfja.com/kjeon0901 라는 서버에서 a라는 웹페이지
+res1 = rq.get(url1)
+print(res1)
+print(res1.status_code)
+'''error'''
 
-res = rq.get(url)
+url2 = "https://github.com/kjeon0901/a" # https://github.com/kjeon0901 라는 서버에서 a라는 웹페이지
+res2 = rq.get(url2)
+print(res2)
+print(res2.status_code)
+'''
+<Response [404]>
+404
 
-print(res)
-print(res.status_code)
+서버 : https://github.com/kjeon0901
+페이지 : https://github.com/kjeon0901/python-spider-machinelearning
+
+url1. 서버 주소 자체를 잘못 입력한 경우 => '사이트에 연결할 수 없음', 코드 상으로는 아예 에러 남. 
+url2. 서버에 들어갔는데 페이지가 없는 경우 => 404 뜸. 
+
+프로그램이 돌다가 찾는 페이지 없어서 넘어가면 괜찮지만, 아예 에러가 나서 멈춰 있으면 안되니까 try-catch문 多 써줌! 
+'''
+
