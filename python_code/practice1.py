95b230c250aafe0a025445af4249059a73de8f89
kjeon0901 <kjeon0901@naver.com>
2021-06-25 09:28:16 +0900

    add - driver.current_url to change relative path to absolute path

--- python_code/practice1.py
+++ python_code/practice1.py
@@ -7,16 +7,15 @@ import time
 url = 'https://naver.com'
 driver = webdriver.Chrome('C:/Users/hs-702/Desktop/kjeon/chromedriver_win32/chromedriver.exe')
 driver.get(url)
-driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[2]/div[1]/div[1]/ul[2]/li[2]/a').click()
+driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[3]/div[1]/div[1]/ul[2]/li[2]/a').click()
+
 bs = BeautifulSoup(driver.page_source)
 
 for idx, category in enumerate(bs.find_all('div', {'class':'main_component droppable'})):
     print("\n======",category.h4.get_text(), "======")
     for title in category.find('ul').find_all('li'):
         if idx==0:
-            print(bs.find('div', {'id':'gnb_search_lstwrp'}))
-            #print(bs.find('ul', class_='gnb_search_lst gnb_first').find('li'))
-            #.next_sibling.a.get('href')+title.a.get("href"))
+            print(driver.current_url+title.a.get("href"))
             print(title.a.get_text())
         else:
             print(title.a.get("href"))
