219dc1fcdabc616d27272480eea6cfca36f1cb11
kjeon0901 <kjeon0901@naver.com>
2021-06-30 09:46:08 +0900

    del - useless selector code

--- python_code/practice.py
+++ python_code/practice.py
@@ -99,11 +99,11 @@ crawler = Crawler()
 
 siteData = [
     ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=',
-        'article.result', 'p.title a', True, 'p[itemprop=summary], article.press-main h2', 'div[itemprop=description]'],
+        'article.result', 'p.title a', True, 'p[itemprop=summary]', 'div[itemprop=description]'],
     ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=', 'div.search-result-content',
         'h3.search-result-title a', False, 'h1', 'div.ArticleBodyWrapper, div.ArticleBody__content___2gQno2.paywall-article'],
     ['Brookings', 'http://www.brookings.edu', 'https://www.brookings.edu/search/?s=',
-        'div.list-content article[class*=has-image]', 'h4.title a', True, 'h1.report-title, a.techstream--title', 'div.post-body.post-body-enhanced']
+        'div.list-content article[class*=has-image]', 'h4.title a', True, 'h1.report-title', 'div.post-body.post-body-enhanced']
 ]
 sites = []
 for row in siteData:
