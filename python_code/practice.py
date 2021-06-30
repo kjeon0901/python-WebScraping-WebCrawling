f9f6a871040321496a54d5c9435a111cbe99219e
kjeon0901 <kjeon0901@naver.com>
2021-06-30 12:51:50 +0900

    add -

--- python_code/practice.py
+++ python_code/practice.py
@@ -1,117 +1,83 @@
-## Crawling through sites with search
+## Crawling Sites through Links
 
-# 1. 전역변수 사용해서 print되는 text들을 전역변수에 담아보기. 
+# Quiz. business 섹션의 기사들만 print
+
+import requests
+from bs4 import BeautifulSoup
 
 #ln [1] :
 
-    
-all_ = []
-
-class Content:
-    """Common base class for all articles/pages"""
-
-    def __init__(self, topic, url, title, body):
-        self.topic = topic
-        self.url = url
-        self.title = title
-        self.body = body
-
-    def print(self):
-        """
-        Flexible printing function controls output
-        """
-        print('New article found for topic: {}'.format(self.topic))
-        print('URL: {}'.format(self.url))
-        print('TITLE: {}'.format(self.title))
-        print('BODY:\n{}'.format(self.body))
-        
-    def print_2(self):
-        return self.topic, self.url, self.title, self.body
-
-
-#ln [2] :
-
 class Website:
-    """Contains information about website structure"""
 
-    def __init__(self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag):
-        self.name = name # 사이트이름
-        self.url = url # 사이트 도메인주소
-        self.searchUrl = searchUrl # 검색창url - 활용도Good!
-        self.resultListing = resultListing # 검색 결과 나온 요소 위치 찾는 태그
-        self.resultUrl = resultUrl # 각 요소의 제목(==링크) 위치 찾는 태그
+    def __init__(self, name, url, targetPattern, absoluteUrl, titleTag, bodyTag):
+        self.name = name # 사이트 이름
+        self.url = url # 사이트 도메인 주소
+        self.targetPattern = targetPattern # 찾아야 하는 href 패턴 (정규표현식)
         self.absoluteUrl = absoluteUrl # 절대주소 : True, 상대주소 : False
         self.titleTag = titleTag # 링크 내부 title 위치 찾는 태그
         self.bodyTag = bodyTag # 링크 내부 body 위치 찾는 태그
 
 
-#ln [3] :
+class Content:
 
-import requests
-from bs4 import BeautifulSoup
+    def __init__(self, url, title, body):
+        self.url = url
+        self.title = title
+        self.body = body
+
+    def print(self):
+        print('URL: {}'.format(self.url))
+        print('TITLE: {}'.format(self.title))
+        print('BODY:\n{}'.format(self.body))
+
+
+#ln [2] :
+
+import re
+
 
 class Crawler:
+    def __init__(self, site):
+        self.site = site
+        self.visited = []
 
     def getPage(self, url):
         try:
-            req = requests.get(url)
+            req = requests.get(url) # url에 해당하는 html 가져옴
         except requests.exceptions.RequestException:
-            return None
-        return BeautifulSoup(req.text, 'html.parser')
+            return None # 에러나면 None 리턴
+        return BeautifulSoup(req.text, 'html.parser') # 그 html을 파싱하기 위한 bs 객체 리턴
 
     def safeGet(self, pageObj, selector):
-        childObj = pageObj.select(selector)
-        if childObj is not None and len(childObj) > 0:
-            return childObj[0].get_text()
-        return ''
+        selectedElems = pageObj.select(selector) 
+        if selectedElems is not None and len(selectedElems) > 0: # 하나 이상인 경우
+            return '\n'.join([elem.get_text() for elem in selectedElems]) # 각 요소의 text 뽑고, 그 요소 사이사이에 '\n'을 넣어 하나의 큰 문자열로 만듦
+        return '' # 아무것도 못 찾은 경우
 
-    def search(self, topic, site):
+    def parse(self, url):
+        bs = self.getPage(url) # 새로운 url에 해당하는 getPage( ) 메소드
+        if bs is not None: # bs 잘 받아왔으면
+            title = self.safeGet(bs, self.site.titleTag) # safeGet( ) 메소드
+            body = self.safeGet(bs, self.site.bodyTag) # safeGet( ) 메소드
+            if title != '' and body != '': # title, body 둘다 잘 가져왔을 때만
+                content = Content(url, title, body)
+                content.print() # 출력
+
+    def crawl(self):
         """
-        Searches a given website for a given topic and records all pages found
+        Get pages from website home page
         """
-        bs = self.getPage(site.searchUrl + topic) # '검색창에 topic을 검색'한 url에 해당하는 html을 파싱하는 객체
-        searchResults = bs.select(site.resultListing) # 검색 결과 검색된 모든 요소에 대한 태그 모두 찾아서 담음
-        for result in searchResults:
-            #try:
-            url = result.select_one(site.resultUrl).attrs['href']
-            # 혹시 하나의 기사 안에 같은 클래스인 여러 태그가 있을 수 있으니까 첫 번째 고름 (select만 하면 여러개 담겨서 attrs['href'] 접근 불가)
-            # Check to see whether it's a relative or an absolute URL
-            if(site.absoluteUrl): # 절대주소면
-                bs = self.getPage(url) # 그대로
-            else: # 상대주소면
-                bs = self.getPage(site.url + url) # 도메인주소 뒤에 붙여 절대주소로 바꿈
-            if bs is None:
-                print('Something was wrong with that page or URL. Skipping!')
-                return
-            title = self.safeGet(bs, site.titleTag)
-            body = self.safeGet(bs, site.bodyTag)
-            #print('===============================================')
-            #print(title, body)
-            if title != '' and body != '': # O\'Reilly 여기서 안들어가짐!!!! => 해결o
-                print('===============================================')
-                content = Content(topic, title, body, url)
-                content.print()
-                all_.append(content.print_2()) # 하나로 받았으므로 튜플로 받아짐
-            #except AttributeError as e:
-            #    print(e)
+        bs = self.getPage(self.site.url) # 도메인주소에 해당하는 getPage( ) 메소드
+        targetPages = bs.findAll('a', href=re.compile(self.site.targetPattern)) # 현재 html에서 href가 targetPattern인 a태그 모두 찾기
+        for targetPage in targetPages: # targetPage(a태그들) 하나씩
+            targetPage = targetPage.attrs['href'] # targetPage에 a태그 대신 href 속성의 value값 넣어줌
+            if targetPage not in self.visited: # 방문한 적 없으면
+                self.visited.append(targetPage) # 방문기록 history에 넣어줌
+                if not self.site.absoluteUrl: # 절대주소가 아니라면 (지금 모두 /article/로 시작하는 상대주소인 상황)
+                    targetPage = '{}{}'.format(self.site.url, targetPage) # 절대주소로 바꿈
+                self.parse(targetPage) # parse( ) 메소드
 
-crawler = Crawler()
 
-siteData = [
-    ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=',
-        'article.result', 'p.title a', True, 'p[itemprop=summary]', 'div[itemprop=description]'],
-    ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=', 'div.search-result-content',
-        'h3.search-result-title a', False, 'h1', 'div.ArticleBodyWrapper, div.ArticleBody__content___2gQno2.paywall-article'],
-    ['Brookings', 'http://www.brookings.edu', 'https://www.brookings.edu/search/?s=',
-        'div.list-content article[class*=has-image]', 'h4.title a', True, 'h1.report-title', 'div.post-body.post-body-enhanced']
-]
-sites = []
-for row in siteData:
-    sites.append(Website(row[0], row[1], row[2],
-                         row[3], row[4], row[5], row[6], row[7]))
-
-topics = ['python', 'data science']
-for topic in topics:
-    print('GETTING INFO ABOUT: ' + topic)
-    for targetSite in sites:
-        crawler.search(topic, targetSite)
+reuters = Website('Reuters', 'https://www.reuters.com', '^(/article/)', False, 'h1', 'div.StandardArticleBody_body_1gnLA') # Website 객체
+crawler = Crawler(reuters) # reuters 를 크롤링하기 위한 Crawler 객체
+crawler.crawl()
--- python_code/practice1.py
+++ python_code/practice1.py
@@ -1,40 +1,117 @@
+## Crawling through sites with search
+
+# Quiz. 전역변수 사용해서 print되는 text들을 전역변수에 담아보기. 
+
+#ln [1] :
+
+    
+all_ = []
+
+class Content:
+    """Common base class for all articles/pages"""
+
+    def __init__(self, topic, url, title, body):
+        self.topic = topic
+        self.url = url
+        self.title = title
+        self.body = body
+
+    def print(self):
+        """
+        Flexible printing function controls output
+        """
+        print('New article found for topic: {}'.format(self.topic))
+        print('URL: {}'.format(self.url))
+        print('TITLE: {}'.format(self.title))
+        print('BODY:\n{}'.format(self.body))
+        
+    def print_2(self):
+        return self.topic, self.url, self.title, self.body
+
+
+#ln [2] :
+
+class Website:
+    """Contains information about website structure"""
+
+    def __init__(self, name, url, searchUrl, resultListing, resultUrl, absoluteUrl, titleTag, bodyTag):
+        self.name = name # 사이트이름
+        self.url = url # 사이트 도메인주소
+        self.searchUrl = searchUrl # 검색창url - 활용도Good!
+        self.resultListing = resultListing # 검색 결과 나온 요소 위치 찾는 태그
+        self.resultUrl = resultUrl # 각 요소의 제목(==링크) 위치 찾는 태그
+        self.absoluteUrl = absoluteUrl # 절대주소 : True, 상대주소 : False
+        self.titleTag = titleTag # 링크 내부 title 위치 찾는 태그
+        self.bodyTag = bodyTag # 링크 내부 body 위치 찾는 태그
+
+
+#ln [3] :
+
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from urllib.request import urlopen
-import time
 
-#### [네이버 뉴스] - 7분야 각각의 탭에서 카테고리 + 5가지 요약문 + 기사별 url(상대경로도 모두 절대경로로) + 기사별 입력날짜데이터 출력, 리스트에 넣기
+class Crawler:
 
-url = 'https://naver.com'
-driver = webdriver.Chrome('C:/Users/hs-702/Desktop/kjeon/chromedriver_win32/chromedriver.exe')
-driver.get(url)
-driver.find_element_by_xpath('/html/body/div[2]/div[2]/div[3]/div[1]/div[1]/ul[2]/li[2]/a').click()
+    def getPage(self, url):
+        try:
+            req = requests.get(url)
+        except requests.exceptions.RequestException:
+            return None
+        return BeautifulSoup(req.text, 'html.parser')
 
-bs = BeautifulSoup(driver.page_source)
-var = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'} # 우회하기 위함
+    def safeGet(self, pageObj, selector):
+        childObj = pageObj.select(selector)
+        if childObj is not None and len(childObj) > 0:
+            return childObj[0].get_text()
+        return ''
 
-total = []
-for idx, category in enumerate(bs.find_all('div', {'class':'main_component droppable'})):
-    category_total = []
-    print("\n======",category.h4.get_text(), "======")
-    for idx2, title in enumerate(category.find('ul').find_all('li')):
-        if idx==0:
-            news_title = title.a.get_text()
-            news_url = driver.current_url + title.a.get("href")
-        else:
-            news_title = title.a.strong.get_text()
-            news_url = title.a.get("href")
-        print(news_title) # 기사별 요약문 출력
-        print(news_url) # 기사별 url(상대경로도 모두 절대경로로) 출력
-        
-        html = requests.get(news_url, headers = var) # var에 담긴 fake user-agent 정보를 headers로 넣어줌. 
-        bs2 = BeautifulSoup(html.text)
-        news_date = bs2.find('span', {'class':'t11'}).get_text()
-        print(news_date) # 기사별 입력날짜데이터 출력
-        time.sleep(0.5)
-        
-        category_total.append([news_title, news_url, news_date]) # 리스트에 담기
-    total.append(category_total)
-        
-#driver.quit()
+    def search(self, topic, site):
+        """
+        Searches a given website for a given topic and records all pages found
+        """
+        bs = self.getPage(site.searchUrl + topic) # '검색창에 topic을 검색'한 url에 해당하는 html을 파싱하는 객체
+        searchResults = bs.select(site.resultListing) # 검색 결과 검색된 모든 요소에 대한 태그 모두 찾아서 담음
+        for result in searchResults:
+            #try:
+            url = result.select_one(site.resultUrl).attrs['href']
+            # 혹시 하나의 기사 안에 같은 클래스인 여러 태그가 있을 수 있으니까 첫 번째 고름 (select만 하면 여러개 담겨서 attrs['href'] 접근 불가)
+            # Check to see whether it's a relative or an absolute URL
+            if(site.absoluteUrl): # 절대주소면
+                bs = self.getPage(url) # 그대로
+            else: # 상대주소면
+                bs = self.getPage(site.url + url) # 도메인주소 뒤에 붙여 절대주소로 바꿈
+            if bs is None:
+                print('Something was wrong with that page or URL. Skipping!')
+                return
+            title = self.safeGet(bs, site.titleTag)
+            body = self.safeGet(bs, site.bodyTag)
+            #print('===============================================')
+            #print(title, body)
+            if title != '' and body != '': # O\'Reilly 여기서 안들어가짐!!!! => 해결o
+                print('===============================================')
+                content = Content(topic, title, body, url)
+                content.print()
+                all_.append(content.print_2()) # 하나로 받았으므로 튜플로 받아짐
+            #except AttributeError as e:
+            #    print(e)
+
+crawler = Crawler()
+
+siteData = [
+    ['O\'Reilly Media', 'http://oreilly.com', 'https://ssearch.oreilly.com/?q=',
+        'article.result', 'p.title a', True, 'p[itemprop=summary]', 'div[itemprop=description]'],
+    ['Reuters', 'http://reuters.com', 'http://www.reuters.com/search/news?blob=', 'div.search-result-content',
+        'h3.search-result-title a', False, 'h1', 'div.ArticleBodyWrapper, div.ArticleBody__content___2gQno2.paywall-article'],
+    ['Brookings', 'http://www.brookings.edu', 'https://www.brookings.edu/search/?s=',
+        'div.list-content article[class*=has-image]', 'h4.title a', True, 'h1.report-title', 'div.post-body.post-body-enhanced']
+]
+sites = []
+for row in siteData:
+    sites.append(Website(row[0], row[1], row[2],
+                         row[3], row[4], row[5], row[6], row[7]))
+
+topics = ['python', 'data science']
+for topic in topics:
+    print('GETTING INFO ABOUT: ' + topic)
+    for targetSite in sites:
+        crawler.search(topic, targetSite)
