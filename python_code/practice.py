ea0a1ac30c989a1db9dd5790a9cbc4f2fb84d965
kjeon0901 <kjeon0901@naver.com>
2021-07-02 11:40:36 +0900

    add - MySQL 8.0

--- python_code/practice.py
+++ python_code/practice.py
@@ -1,14 +1,14 @@
-import os # 내 컴퓨터 안의 경로로 이동한다거나, 폴더를 만든다거나 하는 행위 가능
+import os
 from urllib.request import urlretrieve
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
 downloadDirectory = 'downloaded'
-baseUrl = 'http://pythonscraping.com' # 도메인주소이자, 우리가 scraping할 웹페이지
+baseUrl = 'http://pythonscraping.com'
 
 def getAbsoluteURL(baseUrl, source):
-    if source.startswith('http://www.'): # startswith('str') : source를 전부 string의 관점에서 봤을 때, string이 'str'로 시작하는가?
+    if source.startswith('http://www.'):
         url = 'http://{}'.format(source[11:])
     elif source.startswith('https://www.'):
         url = 'http://{}'.format(source[12:])
@@ -18,30 +18,30 @@ def getAbsoluteURL(baseUrl, source):
         url = 'http://{}'.format(source[8:])
     elif source.startswith('www.'):
         url = 'http://{}'.format(source[4:])
-    else: # source가 상대경로라면
-        url = '{}/{}'.format(baseUrl, source) # 도메인주소와 합쳐줌
-    return url # 결과적으로, 전부 http://로 시작하고 www.는 없는 꼴로 바꿔줌
+    else:
+        url = '{}/{}'.format(baseUrl, source) 
+    return url 
 
 def getDownloadPath(baseUrl, absoluteUrl, downloadDirectory):
-    path = absoluteUrl.replace('www.', '') # 아까 getAbsoluteURL( )에서 이미 다 없애줬기 때문에 의미 없는 코드임
-    path = path.replace(baseUrl, '') # 도메인주소 포함되어 있으면 없애줌 (내 컴퓨터의 디렉토리에 저장해야 하니까) => "상대경로만 남음!!"
-    external = re.compile('.*\.com').search(path) # 외부 도메인주소가 들어가 있으면 없애줌. None 들어가던 것 해결함. 
+    path = absoluteUrl.replace('www.', '')
+    path = path.replace(baseUrl, '') 
+    external = re.compile('.*\.com').search(path) 
     if external != None:
         path = path.replace(external.group(), '')
-        
-    path = downloadDirectory+path # 'downloaded' + 상대경로 => 저장 경로 완성
-    directory = os.path.dirname(path) # path에서 directory만 담음
+    path = downloadDirectory+path 
+    directory = os.path.dirname(path) 
 
-    if not os.path.exists(directory): # directory(폴더)가 존재하지 않으면
-        os.makedirs(directory) # directory(폴더) 새로 만들어줌
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    print(path)
     return path
 
 html = urlopen('http://www.pythonscraping.com')
 bs = BeautifulSoup(html, 'html.parser')
-downloadList = bs.body.findAll(src=True) # src라는 속성값을 가지고 있는 모든 태그    // script에서 쓰인 건 제외되니까, 총 3개의 img파일만 담김
+downloadList = bs.body.findAll(src=True)
 
 for download in downloadList:
-    fileUrl = getAbsoluteURL(baseUrl, download['src']) # getAbsoluteURL( ) 메소드 : 서버 상에 저장된 이미지 파일 경로가 담김
-    print('fileUrl',fileUrl)
-    if fileUrl is not None: # 존재하면
+    fileUrl = getAbsoluteURL(baseUrl, download['src']) 
+    if fileUrl is not None:
+        print(fileUrl)
         urlretrieve(fileUrl, getDownloadPath(baseUrl, fileUrl, downloadDirectory))    
