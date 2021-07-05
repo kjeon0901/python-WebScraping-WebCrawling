8b32485c449c6f254461694ee646a8132ca83afe
kjeon0901 <kjeon0901@naver.com>
2021-07-05 11:10:51 +0900

    add - use several tables -- pages, links add - use recursion add - mysql, csv

--- python_code/practice1.py
+++ python_code/practice1.py
@@ -1,26 +1,61 @@
-import csv
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import pymysql
+from random import shuffle
 
-# 크롤링한 데이터를 csv 파일에 저장하는 코드
+conn = pymysql.connect(host='127.0.0.1',
+                       user='root', passwd='KJEON0901Q1W2E3R4', db='mysql', charset='utf8')
+cur = conn.cursor() # sql문을 실행시키고 결과를 얻어올 때 사용할 커서(cursor) 만듦
+cur.execute('USE wikipedia') # sql문을 실행(execute)
 
-html = urlopen('http://en.wikipedia.org/wiki/Comparison_of_text_editors')
-bs = BeautifulSoup(html, 'html.parser')
-# The main comparison table is currently the first table on the page
-table = bs.findAll('table',{'class':'wikitable'})[0]
-rows = table.findAll('tr')
-'''
-table은 사실상 csv로 저장하기 가장 좋은 형태. 
-애초에 형태 자체가 똑같으므로 어떤 방식으로 저장할지에 대한 고민 없이 그대로 긁어서 저장하면 됨.
-'''
+def insertPageIfNotExists(url):
+    cur.execute('SELECT * FROM pages WHERE url = %s', (url)) # pages 테이블에서 해당 url을 가진 모든 row 가져옴
+    if cur.rowcount == 0: # 하나도 없으면
+        cur.execute('INSERT INTO pages (url) VALUES (%s)', (url)) # pages 테이블에 해당 url을 가진 새로운 추가
+        conn.commit()
+        return cur.lastrowid # db_cursor를 이용해 excute한 테이블의 마지막 행 id 값을 가져옴
+    else: # 있으면
+        return cur.fetchone()[0] # 한 줄 row을 읽어서 0번째 column값을 리턴 -> id 리턴
 
-csvFile = open('editors.csv', 'wt+', encoding='UTF-8') # 인코딩 방식 때문에 에러나는 것 해결 위해 encoding='UTF-8' 추가
-writer = csv.writer(csvFile)
-try:
-    for row in rows:
-        csvRow = []
-        for cell in row.findAll(['td', 'th']): # th : tr의 인덱스같은 존재, td : tr 안의 데이터
-            csvRow.append(cell.get_text())
-        writer.writerow(csvRow) # editors.csv에서 하나의 row에 csvRow 써줌
-finally: # 예외처리 try-except-finally : try문 수행 도중 예외가 발생했든 아니든(try문 수행하든 except문 수행하든) finally문 항상 수행
-    csvFile.close()
+def loadPages():
+    cur.execute('SELECT * FROM pages')
+    pages = [row[1] for row in cur.fetchall()]
+    return pages
+
+def insertLink(fromPageId, toPageId):
+    cur.execute('SELECT * FROM links WHERE fromPageId = %s AND toPageId = %s', 
+                  (int(fromPageId), int(toPageId))) # links 테이블에서 해당 fromPageId, toPageId를 가진 모든 row 가져옴
+    if cur.rowcount == 0: # 하나도 없으면
+        cur.execute('INSERT INTO links (fromPageId, toPageId) VALUES (%s, %s)', 
+                    (int(fromPageId), int(toPageId))) # links 테이블에 해당 fromPageId, toPageId를 갖는 새로운 row 추가
+        conn.commit()
+def pageHasLinks(pageId):
+    cur.execute('SELECT * FROM links WHERE fromPageId = %s', (int(pageId))) # links 테이블에서 해당 pageId를 fromPageId로 가진 모든 row 가져옴
+    rowcount = cur.rowcount
+    if rowcount == 0: # 하나도 없으면
+        return False
+    return True # 하나라도 있으면
+
+def getLinks(pageUrl, recursionLevel, pages): # pageUrl : 현재 웹페이지 주소
+    if recursionLevel > 4: # 재귀 5번 돌고 6번째 끝남. 
+        return
+
+    pageId = insertPageIfNotExists(pageUrl) # pages에서 pageUrl을 url로 가진 첫 번째 row에 담긴 id 가져옴
+    html = urlopen('http://en.wikipedia.org{}'.format(pageUrl))
+    bs = BeautifulSoup(html, 'html.parser') # urlopen으로 열면 html만 넣어줘도 됨
+    links = bs.findAll('a', href=re.compile('^(/wiki/)((?!:).)*$')) # html 안의 모든 내부링크 가진 a태그 찾아서 리턴
+    links = [link.attrs['href'] for link in links] # href 속성값만 빼서 담음
+
+    for link in links: # link(href 속성값) 하나씩
+        linkId = insertPageIfNotExists(link) # pages 테이블에서 link를 url로 가진 첫 번째 row에 담긴 id 가져옴
+        insertLink(pageId, linkId) # links 테이블에 pageId, linkId를 각각 fromPageId, toPageId로 갖는 row가 없다면 추가. 
+        if not pageHasLinks(linkId): # links 테이블에 linkId를 fromPageId로 갖는 row가 하나도 없다면
+            print("PAGE HAS NO LINKS: {}".format(link))
+            pages.append(link) # pages 테이블에 link를 url로 갖는 새로운 row 추가
+            return getLinks(link, recursionLevel+1, pages) # 재귀→계속 첫 번째 내부링크로만 들어감
+        
+        
+getLinks('/wiki/Kevin_Bacon', 0, loadPages()) 
+cur.close()
+conn.close()
